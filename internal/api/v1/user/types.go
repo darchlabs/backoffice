000c6c7e2ab341089ac5924ebdf58d4ba8aa746a
mtavano <chelotavano.udp@gmail.com>
2023-05-16 16:02:14 -0400

    Upate api
    * Add POST login
    * Update POST signup
    * Update types

---
+++ internal/api/v1/user/types.go
@@ -0,0 +1,18 @@
+package user
+
+import (
+	"github.com/darchlabs/backoffice/internal/storage"
+	"github.com/darchlabs/backoffice/internal/storage/auth"
+	"github.com/darchlabs/backoffice/internal/storage/user"
+	userdb "github.com/darchlabs/backoffice/internal/storage/user"
+)
+
+type idGenerate func() string
+
+type userInsertQuery func(storage.QueryContext, *userdb.Record) error
+
+type userSelectByEmailQuery func(storage.Transaction, *user.SelectByEmailQueryData) (*user.Record, error)
+
+type authInsertQuery func(storage.QueryContext, *auth.Record) error
+
+type authUpsertQuery func(storage.QueryContext, *auth.Record) error
