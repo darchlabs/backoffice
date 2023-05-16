607dcbf8ea038d5b7edf8fbd4432092351484941
mtavano <chelotavano.udp@gmail.com>
2023-05-16 16:04:16 -0400

    Update app

--- internal/application/app.go
+++ internal/application/app.go
@@ -1,17 +1,23 @@
 package application
 
-import "github.com/darchlabs/backoffice/internal/storage"
+import (
+	"github.com/darchlabs/backoffice/config"
+	"github.com/darchlabs/backoffice/internal/storage"
+)
 
 type App struct {
+	Config   *config.Config
 	SqlStore storage.SQL
 }
 
 type Config struct {
+	Config   *config.Config
 	SqlStore storage.SQL
 }
 
 func New(conf *Config) (*App, error) {
 	return &App{
+		Config:   conf.Config,
 		SqlStore: conf.SqlStore,
 	}, nil
 }
