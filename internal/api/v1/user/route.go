ea24a95ddf72fec541da1a6f8e6dbbdacacf8a79
mtavano <chelotavano.udp@gmail.com>
2023-05-31 04:25:18 -0400

    Update api

--- internal/api/v1/user/route.go
+++ internal/api/v1/user/route.go
@@ -2,11 +2,15 @@ package user
 
 import (
 	"fmt"
+	"net/http"
 
 	"github.com/darchlabs/backoffice/internal/api/context"
 	v1 "github.com/darchlabs/backoffice/internal/api/v1"
+	"github.com/darchlabs/backoffice/internal/storage/apikey"
 	authdb "github.com/darchlabs/backoffice/internal/storage/auth"
 	userdb "github.com/darchlabs/backoffice/internal/storage/user"
+	"github.com/darchlabs/backoffice/pkg/client"
+	"github.com/darchlabs/backoffice/pkg/middleware"
 	"github.com/google/uuid"
 )
 
@@ -29,8 +33,34 @@ func Route(basePath string, ctx *context.Ctx) {
 		userSelectByEmailQuery: userdb.SelectByEmailQuery,
 	}
 
+	postApiKeyHandler := &PostApiKeyHandler{
+		secretKey:              ctx.App.Config.SecretKey,
+		idGenerate:             uuid.NewString,
+		apikeyInsertQuery:      apikey.InsertQuery,
+		authSelectByTokenQuery: authdb.SelectByTokenQuery,
+	}
+
+	postValidApiKeyHandler := &PostValidApiKeyHandler{
+		secretKey:                ctx.App.Config.SecretKey,
+		apikeySelectByTokenQuery: apikey.SelectByTokenQuery,
+	}
+
+	// setup middleware
+	cl := client.New(&client.Config{
+		Client:  http.DefaultClient,
+		BaseURL: fmt.Sprintf("http://0.0.0.0:%s", ctx.App.Config.ApiServerPort),
+	})
+
+	auth := middleware.NewAuth(cl)
+
 	// route
 	ctx.Server.Post(fmt.Sprintf("%s/signup", basePath), v1.HandleFunc(ctx, postSignupHandler.Invoke))
 	ctx.Server.Post(fmt.Sprintf("%s/login", basePath), v1.HandleFunc(ctx, postLoginHandler.Invoke))
 	ctx.Server.Post(fmt.Sprintf("%s/tokens", basePath), v1.HandleFunc(ctx, postValidTokenHandler.Invoke))
+	ctx.Server.Post(
+		fmt.Sprintf("%s/api-key", basePath),
+		auth.Middleware,
+		v1.HandleFunc(ctx, postApiKeyHandler.Invoke),
+	)
+	ctx.Server.Post(fmt.Sprintf("%s/valid-api-key", basePath), v1.HandleFunc(ctx, postValidApiKeyHandler.Invoke))
 }
--- internal/api/v1/user/types.go
+++ internal/api/v1/user/types.go
@@ -1,10 +1,15 @@
 package user
 
 import (
+	"time"
+
 	"github.com/darchlabs/backoffice/internal/storage"
+	"github.com/darchlabs/backoffice/internal/storage/apikey"
 	"github.com/darchlabs/backoffice/internal/storage/auth"
 	"github.com/darchlabs/backoffice/internal/storage/user"
 	userdb "github.com/darchlabs/backoffice/internal/storage/user"
+	"github.com/golang-jwt/jwt"
+	"github.com/pkg/errors"
 )
 
 type idGenerate func() string
@@ -18,3 +23,57 @@ type authInsertQuery func(storage.QueryContext, *auth.Record) error
 type authUpsertQuery func(storage.QueryContext, *auth.Record) error
 
 type authSelectByTokenQuery func(storage.Transaction, string) (*auth.Record, error)
+
+type apikeyInsertQuery func(storage.QueryContext, *apikey.Record) error
+
+type apikeySelectByTokenQuery func(tx storage.Transaction, token string) (*apikey.Record, error)
+
+func parseToken(secretKey, tokenString string) (*loginClaims, error) {
+	token, err := jwt.ParseWithClaims(tokenString, &loginClaims{}, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, errors.New("unexpected signing method")
+		}
+		return []byte(secretKey), nil
+	})
+
+	if err != nil {
+		return nil, errors.Wrap(err, "jwt.ParseWithClaims error")
+	}
+
+	claims, ok := token.Claims.(*loginClaims)
+	if !ok || !token.Valid {
+		return nil, errors.New("invalid token")
+	}
+
+	// Verify that the token is not expired
+	if claims.ExpiresAt < time.Now().Unix() {
+		return nil, errors.New("token expired")
+	}
+
+	return claims, nil
+}
+
+func parseApiKey(secretKey, tokenString string) (*apiKeyClaims, error) {
+	token, err := jwt.ParseWithClaims(tokenString, &apiKeyClaims{}, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, errors.New("unexpected signing method")
+		}
+		return []byte(secretKey), nil
+	})
+
+	if err != nil {
+		return nil, errors.Wrap(err, "jwt.ParseWithClaims error")
+	}
+
+	claims, ok := token.Claims.(*apiKeyClaims)
+	if !ok || !token.Valid {
+		return nil, errors.New("invalid token")
+	}
+
+	// Verify that the token is not expired
+	if claims.ExpiresAt < time.Now().Unix() {
+		return nil, errors.New("token expired")
+	}
+
+	return claims, nil
+}
